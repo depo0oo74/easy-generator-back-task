1c301cdf2fe0bb3b705f6752f2ec4b0c64a63bff
ahmed eldeeb <a.eldeeb@prosylab.com>
2025-03-09 01:20:24 +0200

    add logger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston'
+import * as winston from 'winston';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function server() {
-  const app = await NestFactory.create(AppModule);
+  const logger = WinstonModule.createLogger({
+    level: 'info',
+    format: winston.format.simple(),
+    transports: [new winston.transports.Console()],
+  });
+  const app = await NestFactory.create(AppModule, {logger});
+  
+  app.use((req, _, next) => {
+    logger.log(
+      `${req.method} ${req.originalUrl} ${JSON.stringify(
+        req.query,
+      )} ${JSON.stringify(req.params)} ${req.statusCode ?? ''}`,
+    );
+    return next();
+  });
   app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Easy Generator API Documentation')
-    .setDescription('This is a Swagger documentation which descripes the endpoints of APIs and there requests and responses')
+    .setDescription('This is a Swagger documentation which descripes the endpoints of APIs and thers and responses')
     .setVersion('1.0')
     .addTag('backend-task')
     .addBearerAuth()
